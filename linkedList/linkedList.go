f692c3ed2684adc65c6f171384272d642acccdbe
dayu <dayu@dayu.jp>
2026-04-06 01:57:32 +0900

    feat(連結リストを作った):

---
+++ linkedList/linkedList.go
@@ -0,0 +1,81 @@
+package linkedlist
+
+type LinkedList[T any] interface {
+	Add(v T)
+	Delete(i int)
+	At(i int) T
+}
+
+type Node[T any] struct {
+	value T
+	next *Node[T]
+}
+
+type HeadLinkedList[T any] struct {
+	head *Node[T]
+}
+
+func NewLinkedList[T any] () LinkedList[T]{
+	return &HeadLinkedList[T]{
+		head: nil,
+	}
+}
+
+func (h *HeadLinkedList[T]) Add(v T) {
+	newNode := &Node[T] {
+		value: v,
+		next: nil,
+	}
+
+	if h.head == nil {
+		h.head = newNode
+		return
+	}
+
+	pos := h.head
+	for pos.next != nil {
+		pos = pos.next
+	}
+
+	pos.next = newNode
+}
+
+func (h *HeadLinkedList[T]) Delete(n int) {
+	if n < 0 || h.head == nil {
+		panic("Index out of range.")
+	}
+
+	if n == 0 {
+		h.head = h.head.next
+		return
+	}
+
+	pos := h.head
+	var former *Node[T]
+
+	for i := 0; i < n; i++ {
+		former = pos
+		pos = pos.next
+		if(pos == nil) {
+			panic("Index out of range.")
+		}
+	}
+
+	former.next = pos.next
+}
+
+func (h *HeadLinkedList[T]) At(n int) T {
+	if n < 0 || h.head == nil {
+		panic("Index out of range.")
+	}
+
+	pos := h.head
+	for i := 0; i < n; i++ {
+		pos = pos.next
+		if(pos == nil) {
+			panic("Index out of range.")
+		}
+	}
+	
+	return pos.value
+}
--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"github.com/dayu255/data-structure/stack"
+	"github.com/dayu255/data-structure/linkedList"
 	"fmt"
 )
 
@@ -14,4 +15,15 @@ func main() {
 	fmt.Println(s.Top())
 	s.Pop()
 	fmt.Println(s.Top())
+
+	l := linkedlist.NewLinkedList[int]()
+
+	l.Add(1)
+	l.Add(2)
+	l.Add(3)
+
+	fmt.Println(l.At(0))
+	fmt.Println(l.At(1))
+	fmt.Println(l.At(2))
+	fmt.Println(l.At(5))
 }
